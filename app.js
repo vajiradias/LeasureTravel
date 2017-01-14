0c17abc4ca15b4487acbdc12d44793e6afbd6318
Vajira Dias <vajira.dias@redflex.com.au>
2017-01-14 11:34:25 +1100

    adding routes to about and home and logo

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var home = require('./routes/home');
+var about = require('./routes/about');
 
 var app = express();
 
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/home', home);
+app.use('/about', about);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
